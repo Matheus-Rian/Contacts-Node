2d6691045466fdbaaf8f033f1080b93cd078ef3e
Matheus Rian <souzamatheusrian@gmail.com>
2021-07-14 12:28:06 -0300

    feat: add method store ✨

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,8 +19,26 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
-    // criar um registro
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExistsEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactExistsEmail) {
+      return response.status(400).json({ error: 'This e-mail is already been taken' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   update() {

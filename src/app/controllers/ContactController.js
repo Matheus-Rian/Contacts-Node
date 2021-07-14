99354ba13a9644652969e7cce48c22d85e2ab58c
Matheus Rian <souzamatheusrian@gmail.com>
2021-07-13 23:11:39 -0300

    feat: ContactController ✨

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,28 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+
+class ContactController {
+  async index(request, response) {
+    const contacts = await ContactsRepository.findAll();
+
+    response.json(contacts);
+  }
+
+  show() {
+    // Obter um registro
+  }
+
+  store() {
+    // criar um registro
+  }
+
+  update() {
+    // Editar um registro
+  }
+
+  delete() {
+    // Deletar um registro
+  }
+}
+
+// Singleton
+module.exports = new ContactController();

58f4cb9212a19c64acdc236acfa572003abdfa0e
Matheus Rian <souzamatheusrian@gmail.com>
2021-09-15 22:21:51 -0300

    feat: middleware cors ✨

---
+++ src/app/middlewares/cors.js
@@ -0,0 +1,7 @@
+// Wildcard -> Curinga(Torna a API public)
+// response.setHeader('Access-Control-Allow-Origin', '*');
+
+module.exports = (request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  next();
+};
